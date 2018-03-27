a38b455416903776f0ce2f0261964d9445fb90ed
Thomas <urfin78@users.noreply.github.com>
2018-03-27 13:32:02 +0200

    removed filepattern

--- ftpsync.sh
+++ ftpsync.sh
@@ -8,7 +8,6 @@ TEMPDIR=''
 FTPSERVER='' #ftp server
 FTPUSER='' #ftp user
 FTPPASS='' #ftp password
-FILEPATTERN='' #regex for filelist
 
 function logging (){
   echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> ${LOGFILE}
@@ -34,12 +33,12 @@ fi
 cd ${TEMPDIR}
 
 wget --quiet -r -nH --no-remove-listing --spider ftp://${FTPUSER}:${FTPPASS}@${FTPSERVER}/${SOURCEDIR}/
-FILELIST=`find . -name '.listing' -type f -print0|xargs -0 grep ${FILEPATTERN}|sed -rs 's/(^\.\/)(.*)\.listing\:.*([[:digit:]]{2}\:[[:digit:]]{2}[[:space:]])(.*)\\r$/\2\4/'`
+FILELIST=`find . -name '.listing' -type f -print0|xargs -0 grep -e '\..*' |sed -rs 's/(^\.\/)(.*)\.listing\:.*([[:digit:]]{2}\:[[:digit:]]{2}[[:space:]])(.*)\\r$/\2\4/'`
 
 cd ${TARGETDIR}
 
 for FILE in ${FILELIST}; do
-  curl -s --create-dirs ftp://${FTPUSER}:${FTPPASS}@${FTPSERVER}/${FILE} -o ${TARGETDIR}/${FILE}.PART
+  curl -s --create-dirs ftp://${FTPUSER}:${FTPPASS}@${FTPSERVER}/${FILE} -o ${TARGETDIR}/${FILE}.part
   if [ $? -ne 0 ]; then
     logging "problem getting ${FTPSERVER}/${FILE}"
   else
